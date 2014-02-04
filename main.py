47d2e7db2985943a4d72f9cc9d00e397cc067111
jd <NikeLambert@gmail.com>
2014-02-04 18:41:13 +0400

    Добавили в art.py новый вид человечков

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def	display(p1, p2):
 	print('Precision'.ljust(length), '[', p1.precision, '%]', '    ', tab,  'Precision'.ljust(length), '[', p2.precision, '%]')
 	print('Punch'.ljust(length), '[ ', p1.hand, ' ]', '    ', tab, 'Punch'.ljust(length), '[ ', p2.hand, ' ]')
 	print('Kick'.ljust(length), '[ ', p1.leg, ' ]', '    ', tab, 'Kick'.ljust(length), '[ ', p1.leg, ' ]')
-	print(person_new)
+	print(person_big)
 
 p1 = Person('John', 10, 70, 4, 6)
 p2 = Person('Bot', 10, 70, 4, 6)
