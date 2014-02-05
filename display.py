0100fdacd138a5d5e6430dd002b691e61721d3c5
jd <NikeLambert@gmail.com>
2014-02-05 21:45:18 +0400

    Вынесли функцию display

---
+++ display.py
@@ -0,0 +1,14 @@
+from art import *
+
+def	display(p1, p2):
+	length = 9
+	tab = '                                       '
+	print('Name'.ljust(length), p1.name, '       ', tab, 'Name'.ljust(length), p2.name)
+	print('Health'.ljust(length), '[', p1.health, ' ]', '    ', tab ,'Health'.ljust(length), '[', p2.health   , ' ]')
+	print('Precision'.ljust(length), '[', p1.precision, '%]', '    ', tab,  'Precision'.ljust(length), '[', p2.precision, '%]')
+	print('Punch'.ljust(length), '[ ', p1.hand, ' ]', '    ', tab, 'Punch'.ljust(length), '[ ', p2.hand, ' ]')
+	print('Kick'.ljust(length), '[ ', p1.leg, ' ]', '    ', tab, 'Kick'.ljust(length), '[ ', p1.leg, ' ]')
+	p1.is_dead()
+	print(tab, end = '')
+	p2.is_dead()
+	print(person_big)
