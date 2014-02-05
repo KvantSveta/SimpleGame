fb51acdd4ee3f788720026f22d368f2095cb8c8f
jd <NikeLambert@gmail.com>
2014-02-05 10:42:48 +0400

    Новый коммит. Во всех файлах изменения внесены

--- classperson.py
+++ classperson.py
@@ -11,8 +11,22 @@ class Person:
 	def punch(self, enemy):
 		if random() < self.precision + 0.1:
 			enemy.health -= self.hand
+			if self.health < 0:
+				self.health = 0
 	
 	def kick(self, enemy):		
-		if random() < self.precision:
+		if random() < self.precision - 0.17:
 			enemy.health -= self.leg
+			if self.health < 0:
+				self.health = 0
+
+	def block(self, enemy):
+		if random() > self.precision + 0.2:
+			if enemy.punch():
+				pass
+
+	def is_dead(self):
+		if self.health == 0:
+			print('You are died', end = '')
+
 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from classperson import Person
 from art import *
+from random import randint 
 
 def	display(p1, p2):
 	length = 9
@@ -11,11 +12,38 @@ def	display(p1, p2):
 	print('Precision'.ljust(length), '[', p1.precision, '%]', '    ', tab,  'Precision'.ljust(length), '[', p2.precision, '%]')
 	print('Punch'.ljust(length), '[ ', p1.hand, ' ]', '    ', tab, 'Punch'.ljust(length), '[ ', p2.hand, ' ]')
 	print('Kick'.ljust(length), '[ ', p1.leg, ' ]', '    ', tab, 'Kick'.ljust(length), '[ ', p1.leg, ' ]')
+	p1.is_dead()
+	print(tab, end = '')
+	p2.is_dead()
 	print(person_big)
 
-p1 = Person('John', 10, 70, 4, 6)
-p2 = Person('Bot', 10, 70, 4, 6)
+def event(p1, p2):
+	action = int(input('Действие: '))
 
-display(p1, p2)
+	if action == 1:
+		p1.punch(p2)
+	elif action == 2:
+		p1.kick(p2)
+	elif action == 3:
+		p1.block(p2)
+	
+def event_2(p1, p2):
+	action = randint(1, 3)
 
-p1.punch(p2)
+	if action == 1:
+		p1.punch(p2)
+	elif action == 2:
+		p1.kick(p2)
+	elif action == 3:
+		p1.block(p2)
+
+p1 = Person('John', 10, 70, 2, 3)
+p2 = Person('Bot', 10, 70, 2, 3)
+
+while True:
+	display(p1, p2)
+	
+	print('Выберите действие: 1 - удар рукой, 2 - удар ногой, 3 - блок')
+		
+	event(p1, p2)
+	event_2(p2, p1)
