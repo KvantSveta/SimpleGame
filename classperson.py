8d7a1e7ab8a21d588ea46a9b101b9df606884a49
jd <NikeLambert@gmail.com>
2014-02-06 19:29:01 +0400

    Опять что-то изменил

--- classperson.py
+++ classperson.py
@@ -2,29 +2,32 @@ from random import randint
 from os import _exit
 
 class Person:
-	def __init__(self, name, health, precision, hand, leg):
+	def __init__(self, name, health, precision, hand, leg, endurance):
 		self.name = name
 		self.health = health
 		self.precision = precision
 		self.hand = hand
 		self.leg = leg
+		self.endurance = endurance
 
 	def punch(self, enemy):
+		self.endurance -= 2
 		if randint(1, 100) < self.precision + 10:
 			enemy.health -= self.hand
 			if enemy.health < 0:
 				enemy.health = 0
 	
-	def kick(self, enemy):		
+	def kick(self, enemy):
+		self.endurance -= 3		
 		if randint(1, 100) < self.precision - 17:
 			enemy.health -= self.leg
 			if enemy.health < 0:
 				enemy.health = 0
 
-	def block(self, enemy):
+	def block(self):
+		self.endurance += 1
 		if randint(1, 100) > self.precision + 20:
-			if enemy.punch():
-				pass
+			pass
 
 	def is_dead(self):
 		if self.health == 0:
--- main.py
+++ main.py
@@ -11,20 +11,20 @@ def event(p1, p2):
 	elif action == 2:
 		p1.kick(p2)
 	elif action == 3:
-		p1.block(p2)
+		p1.block()
 	
-def event_2(p1, p2):
+def event_2(p2, p1):
 	action = randint(1, 3)
 
 	if action == 1:
-		p1.punch(p2)
+		p2.punch(p1)
 	elif action == 2:
-		p1.kick(p2)
+		p2.kick(p1)
 	elif action == 3:
-		p1.block(p2)
+		p2.block()
 
-p1 = Person('John', 10, 70, 2, 3)
-p2 = Person('Bot', 10, 70, 2, 3)
+p1 = Person('John', 10, 70, 2, 3, 10)
+p2 = Person('Bot', 10, 70, 2, 3, 10)
 
 while True:
 	display(p1, p2)
