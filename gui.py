6fe7752147c3504501337e81f88f68904eb63993
jd <NikeLambert@gmail.com>
2014-03-03 01:40:35 +0400

    Изменили gui.py
    Добавили кнопку с надписью, закрывающая окно

--- gui.py
+++ gui.py
@@ -1,10 +1,19 @@
+#!/usr/bin/python3.3
+
+__author__ = 'j.d.'
+
 from tkinter import *
+import sys
 
 root = Tk()
 
-widget = Label(root)
-widget.config(text = 'Hello GUI world!')
-widget.pack(side = TOP, expand = YES, fill = BOTH)
+label = Label(root)
+label.config(text = 'Hello GUI world!')
+label.pack(side = TOP, expand = YES, fill = BOTH)
+
+button = Button(root, text = 'Hello widget world', command = sys.exit)
+# command = root.quit
+button.pack(side = LEFT, expand = YES)
 
 root.title('Simple Game')
 root.mainloop()
