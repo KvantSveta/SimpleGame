66d2e325e592bdbb302ac275d1233993ba354689
jd <NikeLambert@gmail.com>
2014-03-04 10:00:07 +0400

    Изменен gui.py
    Добавлен вывоб "изображения" боя

--- gui.py
+++ gui.py
@@ -27,14 +27,20 @@ label1.pack(anchor = W)
 label2 = Label(frame, text = 'Health'.ljust(length) + '[ ' + str(p1.health).rjust(2) + ' ]' + tab + 'Health'.ljust(length) + '[' + str(p2.health).rjust(2) + ' ]')
 label2.pack(anchor = W)
 
-label3 = Label(frame, text = 'Punch'.ljust(length) + '[ ' + str(p1.hand) + ' ]' + tab + 'Punch'.ljust(length) + '[ ' + str(p2.hand) + ' ]')
+label3 = Label(frame, text = 'Precision'.ljust(length) + '[' + str(p1.precision) + '%]' + tab +  'Precision'.ljust(length) + '[' + str(p2.precision) + '%]')
 label3.pack(anchor = W)
 
-label4 = Label(frame, text = 'Kick'.ljust(length) + '[ ' + str(p1.leg) + ' ]' + tab + 'Kick'.ljust(length) + '[ ' + str(p2.leg) + ' ]')
+label4 = Label(frame, text = 'Punch'.ljust(length) + '[ ' + str(p1.hand) + ' ]' + tab + 'Punch'.ljust(length) + '[ ' + str(p2.hand) + ' ]')
 label4.pack(anchor = W)
 
-label5 = Label(frame, text = 'Endurance'.ljust(length) + '[' + str(p1.endurance).rjust(2) + ' ]' + tab + 'Endurance'.ljust(length) + '[' + str(p2.endurance).rjust(2) + ' ]')
+label5 = Label(frame, text = 'Kick'.ljust(length) + '[ ' + str(p1.leg) + ' ]' + tab + 'Kick'.ljust(length) + '[ ' + str(p2.leg) + ' ]')
 label5.pack(anchor = W)
+
+label6 = Label(frame, text = 'Endurance'.ljust(length) + '[' + str(p1.endurance).rjust(2) + ' ]' + tab + 'Endurance'.ljust(length) + '[' + str(p2.endurance).rjust(2) + ' ]')
+label6.pack(anchor = W)
+
+label7 = Label(frame, text = person_start)
+label7.pack(anchor = W)
 
 button1 = Button(frame, text = 'Удар рукой', command = quit)
 button1.pack(side = LEFT, expand = YES, fill = X)
