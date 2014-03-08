a562abc216e0d1bae2c7be92ce970c96ed6d3c94
jd <NikeLambert@gmail.com>
2014-03-08 16:51:20 +0400

    Изменен gui.py
    Вместо метода .pack() используется метод .grid()

--- gui.py
+++ gui.py
@@ -5,7 +5,6 @@ __author__ = 'j.d.'
 from tkinter import *
 import sys
 from main import *
-#from display import display
 
 def quit():
 	print('Hello, I must be going...')
@@ -13,51 +12,49 @@ def quit():
 
 window = Tk()
 window.title('Simple Game')
-
+'''
 win1 = Toplevel()
 lab1 = Label(win1, text = 'Здесь будет обображена стратегия бота')
 lab1.pack()
-
-frame = Frame()
-frame.pack()
+'''
+frame_1 = Frame()
+frame_1.grid()
 
 length = 11
 tab = ' ' * 44
 
-label1 = Label(frame, text = 'Name'.ljust(length) + p1.name.ljust(7) + tab + 'Name'.ljust(length) + p2.name.ljust(7))
-label1.pack(anchor = W)
-
-label2 = Label(frame, text = 'Health'.ljust(length) + '[ ' + str(p1.health).rjust(2) + ' ]' + tab + 'Health'.ljust(length) + '[' + str(p2.health).rjust(2) + ' ]')
-label2.pack(anchor = W)
-
-label3 = Label(frame, text = 'Precision'.ljust(length) + '[' + str(p1.precision) + '%]' + tab +  'Precision'.ljust(length) + '[' + str(p2.precision) + '%]')
-label3.pack(anchor = W)
-
-label4 = Label(frame, text = 'Punch'.ljust(length) + '[ ' + str(p1.hand) + ' ]' + tab + 'Punch'.ljust(length) + '[ ' + str(p2.hand) + ' ]')
-label4.pack(anchor = W)
-
-label5 = Label(frame, text = 'Kick'.ljust(length) + '[ ' + str(p1.leg) + ' ]' + tab + 'Kick'.ljust(length) + '[ ' + str(p2.leg) + ' ]')
-label5.pack(anchor = W)
-
-label6 = Label(frame, text = 'Endurance'.ljust(length) + '[' + str(p1.endurance).rjust(2) + ' ]' + tab + 'Endurance'.ljust(length) + '[' + str(p2.endurance).rjust(2) + ' ]')
-label6.pack(anchor = W)
+Label(frame_1, text = 'Name'.ljust(length) + p1.name.ljust(7)).grid(row = 0, column = 0)
+Label(frame_1, text = 'Name'.ljust(length) + p2.name.ljust(7)).grid(row = 0, column = 10)
+Label(frame_1, text = 'Health'.ljust(length) + '[ ' + str(p1.health).rjust(2) + ' ]').grid(row = 1, column = 0)
+Label(frame_1, text = 'Health'.ljust(length) + '[' + str(p2.health).rjust(2) + ' ]').grid(row = 1, column = 10)
+Label(frame_1, text = 'Precision'.ljust(length) + '[' + str(p1.precision) + '%]').grid(row = 2, column = 0)
+Label(frame_1, text = 'Precision'.ljust(length) + '[' + str(p2.precision) + '%]').grid(row = 2, column = 8)
+Label(frame_1, text = 'Punch'.ljust(length) + '[ ' + str(p1.hand) + ' ]').grid(row = 3, column = 0)
+Label(frame_1, text = 'Punch'.ljust(length) + '[ ' + str(p2.hand) + ' ]').grid(row = 3, column = 10)
+Label(frame_1, text = 'Kick'.ljust(length) + '[ ' + str(p1.leg) + ' ]').grid(row = 4, column = 0) 
+Label(frame_1, text = 'Kick'.ljust(length) + '[ ' + str(p2.leg) + ' ]').grid(row = 4, column = 6)
+Label(frame_1, text = 'Endurance'.ljust(length) + '[' + str(p1.endurance).rjust(2) + ' ]').grid(row = 5, column = 0)
+Label(frame_1, text = 'Endurance'.ljust(length) + '[' + str(p2.endurance).rjust(2) + ' ]').grid(row = 5, column = 4)
 
 image = PhotoImage(file = './Image/' + 'person_start.gif')
-Label(frame, image = image).pack()
+Label(window, image = image).grid()
 
-button1 = Button(frame, text = 'Удар рукой', command = p1.punch(p2))
-button1.pack(side = LEFT, expand = YES, fill = X)
+frame_2 = Frame()
+frame_2.grid()
 
-button2 = Button(frame, text = 'Удар ногой', command = p1.kick(p2))
-button2.pack(side = LEFT, expand = YES, fill = X)
+button1 = Button(frame_2, text = 'Удар рукой', command = p1.punch(p2))
+button1.grid(row = 0, column = 0)
 
-button3 = Button(frame, text = '   Блок   ', command = p1.block())
-button3.pack(side = LEFT, expand = YES, fill = X)
+button2 = Button(frame_2, text = 'Удар ногой', command = p1.kick(p2))
+button2.grid(row = 0, column = 1)
 
-button4 = Button(frame, text = '   Ждать  ', command = p1.wait())
-button4.pack(side = LEFT, expand = YES, fill = X)
+button3 = Button(frame_2, text = '   Блок   ', command = p1.block())
+button3.grid(row = 0, column = 2)
 
-button5 = Button(frame, text = '   Выйти  ', command = frame.quit)
-button5.pack(side = LEFT, expand = YES, fill = X)
+button4 = Button(frame_2, text = '   Ждать  ', command = p1.wait())
+button4.grid(row = 0, column = 3)
 
-frame.mainloop()
+button5 = Button(frame_2, text = '   Выйти  ', command = frame_2.quit)
+button5.grid(row = 0, column = 4)
+
+window.mainloop()
