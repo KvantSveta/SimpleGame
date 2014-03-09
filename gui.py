dba7badcd5854491aac957e258cb0d838aa5b79c
jd <NikeLambert@gmail.com>
2014-03-09 08:10:59 +0400

    Изменен gui.py
    Почти полностью допилен интерфейс игры

--- gui.py
+++ gui.py
@@ -20,21 +20,22 @@ lab1.pack()
 frame_1 = Frame()
 frame_1.grid()
 
-length = 11
-tab = ' ' * 44
+length = 12
+tab = ' ' * 90
 
 Label(frame_1, text = 'Name'.ljust(length) + p1.name.ljust(7)).grid(row = 0, column = 0)
-Label(frame_1, text = 'Name'.ljust(length) + p2.name.ljust(7)).grid(row = 0, column = 10)
-Label(frame_1, text = 'Health'.ljust(length) + '[ ' + str(p1.health).rjust(2) + ' ]').grid(row = 1, column = 0)
-Label(frame_1, text = 'Health'.ljust(length) + '[' + str(p2.health).rjust(2) + ' ]').grid(row = 1, column = 10)
-Label(frame_1, text = 'Precision'.ljust(length) + '[' + str(p1.precision) + '%]').grid(row = 2, column = 0)
-Label(frame_1, text = 'Precision'.ljust(length) + '[' + str(p2.precision) + '%]').grid(row = 2, column = 8)
-Label(frame_1, text = 'Punch'.ljust(length) + '[ ' + str(p1.hand) + ' ]').grid(row = 3, column = 0)
-Label(frame_1, text = 'Punch'.ljust(length) + '[ ' + str(p2.hand) + ' ]').grid(row = 3, column = 10)
-Label(frame_1, text = 'Kick'.ljust(length) + '[ ' + str(p1.leg) + ' ]').grid(row = 4, column = 0) 
-Label(frame_1, text = 'Kick'.ljust(length) + '[ ' + str(p2.leg) + ' ]').grid(row = 4, column = 6)
-Label(frame_1, text = 'Endurance'.ljust(length) + '[' + str(p1.endurance).rjust(2) + ' ]').grid(row = 5, column = 0)
-Label(frame_1, text = 'Endurance'.ljust(length) + '[' + str(p2.endurance).rjust(2) + ' ]').grid(row = 5, column = 4)
+Label(frame_1, text = tab).grid(row = 0, column = 1)
+Label(frame_1, text = 'Name'.ljust(length) + p2.name.ljust(7)).grid(row = 0, column = 2)
+Label(frame_1, text = 'Health'.ljust(length + 2) + '[ ' + str(p1.health).rjust(2) + ' ]').grid(row = 1, column = 0)
+Label(frame_1, text = 'Health'.ljust(length + 2) + '[ ' + str(p2.health).rjust(2) + ' ]').grid(row = 1, column = 2)
+Label(frame_1, text = 'Precision'.ljust(length + 1) + '[ ' + str(p1.precision) + '%]').grid(row = 2, column = 0)
+Label(frame_1, text = 'Precision'.ljust(length + 1) + '[ ' + str(p2.precision) + '%]').grid(row = 2, column = 2)
+Label(frame_1, text = 'Punch'.ljust(length + 4) + '[ ' + str(p1.hand) + ' ]').grid(row = 3, column = 0)
+Label(frame_1, text = 'Punch'.ljust(length + 4) + '[ ' + str(p2.hand) + ' ]').grid(row = 3, column = 2)
+Label(frame_1, text = 'Kick'.ljust(length + 6) + '[ ' + str(p1.leg) + ' ]').grid(row = 4, column = 0) 
+Label(frame_1, text = 'Kick'.ljust(length + 6) + '[ ' + str(p2.leg) + ' ]').grid(row = 4, column = 2)
+Label(frame_1, text = 'Endurance'.ljust(length) + '[ ' + str(p1.endurance).rjust(2) + ' ]').grid(row = 5, column = 0)
+Label(frame_1, text = 'Endurance'.ljust(length) + '[ ' + str(p2.endurance).rjust(2) + ' ]').grid(row = 5, column = 2)
 
 image = PhotoImage(file = './Image/' + 'person_start.gif')
 Label(window, image = image).grid()
