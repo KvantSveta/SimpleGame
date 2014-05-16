10d1560a3993aed5fd35af5f037cdf22c162eba7
jd <NikeLambert@gmail.com>
2014-05-16 09:55:48 +0400

    Изменен sgserver.py
    Добавлена обработка исключения на случай отсутствия подключения по TLSv1_2

--- sgserver.py
+++ sgserver.py
@@ -13,7 +13,11 @@ from time import sleep
 from classperson import Person
 from logicbot import *
 
-context = SSLContext(PROTOCOL_TLSv1_2)
+try:
+	context = SSLContext(PROTOCOL_TLSv1_2)
+except NameError:
+	context = SSLContext(PROTOCOL_TLSv1)
+
 context.load_cert_chain(certfile='cert.pem', keyfile='cert.pem')
 
 sockobj = socket(AF_INET, SOCK_STREAM)
